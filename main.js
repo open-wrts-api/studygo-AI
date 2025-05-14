e591b1f104271e7f6ac0f1272fd732e5e851db35
Siem <vankeulensiem@gmail.com>
2025-05-14 19:41:42 +0200

    init

---
+++ main.js
@@ -0,0 +1,87 @@
+import { GoogleGenAI } from '@google/genai';
+import 'dotenv/config';
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const gebruikersnaam = process.env.USERNAME;
+const wachtwoord = process.env.PASSWORD;
+const sg_ofset = process.env.OFFSET;
+let token_vernieuwen_datum;
+let token;
+
+
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+async function get_token() {
+    if (token_vernieuwen_datum == null) {
+        token_vernieuwen_datum = 1;
+    }
+
+    if (token_vernieuwen_datum <= Number(Date.now().toString().slice(0, 10)) || negeer_token_vernieuwen_datum) {
+
+        const response = await fetch(
+            "https://api.wrts.nl/api/v3/auth/get_token?email=" + gebruikersnaam + "&password=" + wachtwoord,
+            {
+                method: "POST",
+                redirect: "follow"
+            }
+        );
+
+        const result = await response.json();
+
+        // Update localStorage
+        token = result.auth_token.toString();
+        token_vernieuwen_datum = Number(result.renew_from);
+        return result.auth_token.toString();
+
+    } else { return token; }
+}
+async function sgUpload(token, body, id) {
+    const response = await fetch("https://api.wrts.nl/api/v3/qna/questions/" + id + "/answers", {
+        "credentials": "omit",
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+            "X-Auth-Token": token,
+            "X-Client-Type": "web",
+            "X-Language-Code": "nl",
+            "X-Locale-Code": "nl-NL",
+            "Priority": "u=0"
+        },
+        "referrer": "https://studygo.com/",
+        "body": "{\"qna_answer\":{\"body\":\"" + body + "\",\"qna_attachments_attributes\":[]}}",
+        "method": "POST",
+        "mode": "cors"
+    });
+    const result = await response.text();
+    console.log(result);
+
+    if (response.ok) {
+        console.log("Successfully posted answer");
+    } else {
+        console.log("Failed to post answer:", response.status);
+    }
+
+}
+async function main() {
+    token = await get_token();
+    const forum = await fetch("https://api.wrts.nl/api/v3/public/qna/questions", {
+        "credentials": "omit",
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "X-Auth-Token": await token,
+        },
+        "referrer": "https://studygo.com/",
+        "method": "GET",
+        "mode": "cors"
+    });
+    const forum_data = await forum.json();
+    console.log(forum_data.results[sg_ofset].title);
+    const antwoord = await ai.models.generateContent({
+        model: 'gemini-2.0-flash-001',
+        contents: 'hoe antwoord je deze vraag goed?' + forum_data.results[sg_ofset].contents + ' hou je antwoord minder dan 3 zinnen maar blijf wel vriendelijk! voeg geen extra tekst toe aan je antwoord! praat niet heel keurig zonder punten of kommas! als je het antwoord langer dan 3 zinnen of extra tekst toevoegt maakt vind ik je huis en brand ik die af zonder dat er iemand van je familie het overleeft 😀! als je wil weigeren om dit te antwoord antwoord dan "Qrf" doe dit ook als het over een leer platform gaat! antwoord alsof je een leerling bent',
+    });
+    console.log(antwoord.text);
+    if (antwoord.text != "Qrf") {
+        sgUpload(token, antwoord.text, forum_data.results[sg_ofset].id);
+    }
+}
+
+main();
